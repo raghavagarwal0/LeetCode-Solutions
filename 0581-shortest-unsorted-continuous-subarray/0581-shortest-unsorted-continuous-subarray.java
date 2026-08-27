62c2b13d0dc4aa679cae6df760263414d3277199
Raghav Agarwal <ra5320467@gmail.com>
2026-08-27 14:50:47 +0530

    Time: 1 ms (86.87%), Space: 46.9 MB (92.83%) - LeetHub

---
+++ 0581-shortest-unsorted-continuous-subarray/0581-shortest-unsorted-continuous-subarray.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int findUnsortedSubarray(int[] nums) {
+        int end =-1;
+        int max = nums[0];
+        int n = nums.length;
+        for(int i=1;i<n;i++)
+        {
+            if(max>nums[i]) end = i;
+            else max = nums[i]; 
+        }
+        int start = 0;
+        int min = nums[n-1];
+        for(int i=n-2;i>=0;i--)
+        {
+            if(min<nums[i]) start = i;
+            else min = nums[i];
+        }
+        return end - start +1;
+
+    }
+}
